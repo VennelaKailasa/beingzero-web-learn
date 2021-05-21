a4d76c2f8fd57ac0540a098b640aebda9576a2d6
Vennela Kailasa <vennelakailasa@gmail.com>
2021-05-21 14:45:53 +0530

    tambola

--- frontend/js/tambola.js
+++ frontend/js/tambola.js
@@ -5,35 +5,34 @@ $(document).ready(function(){
 
     })
     var numbers=[];
-    var numslen=100;
-    for(let i=1;i<101;i++)
+    var numslen=90;
+    for(let i=1;i<91;i++)
     numbers.push(i);
     $("#numbtn").click(function(){
         if(numslen==0)
         alert("Click reset to restart the game");
         else{
             var pos=Math.floor(Math.random()*numslen);
+            console.log(pos);
             var n=numbers[pos];
             $("#num").html(n);
             numbers[pos]=numbers[numslen-1];
             numslen--;
-            $("#"+n).css("background-color","black");
-
-
-        }
+            $("#"+n).html(n);
+            }
     })
     $("#resetbtn").click(function(){
         
-        for(let i=1;i<101;i++)
+        for(let i=1;i<91;i++)
         numbers[i-1]=i;
-        numslen=100;
+        numslen=90;
         $("#num").html("0");
-        for(let i=1;i<101;i++)
+        for(let i=1;i<91;i++)
         {
         $("#"+i).css("background-color","white");
+        $("#"+i).html("");
         }
         $(".container").hide()
-
     })
 
 });
