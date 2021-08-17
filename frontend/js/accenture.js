c98386b914a78f92a8d53111b2aed627a18777a0
Vennela Kailasa <vennelakailasa@gmail.com>
2021-08-17 20:29:12 +0530

    ic

--- frontend/js/accenture.js
+++ frontend/js/accenture.js
@@ -3,11 +3,9 @@ $('#signin_button').click(function(){
     console.log('in here')
     window.location='https://vennela-webdev.herokuapp.com/signin';
  });
+ $('#signin_button').click(function(){
+    console.log('in here')
+    window.location='https://vennela-webdev.herokuapp.com/signin';
+ });
 });
 
-$(document).ready(function(){
-    $('#register_button').click(function(){
-        console.log('in here')
-        window.location='https://vennela-webdev.herokuapp.com/register';
-     });
-    });
